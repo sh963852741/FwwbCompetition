98cef03f6aacb5bd4b8e24c6f6a034f492779df1
宋润涵 <53166321+sh963852741@users.noreply.github.com>
2020-02-28 22:52:07 +0800

    绑定页面

--- src/pages/Modules/WorkcellManage/fixDefForm.vue
+++ src/pages/Modules/WorkcellManage/fixDefForm.vue
@@ -1,7 +1,87 @@
 <template>
-    <div/>
+    <i-row>
+        <i-form>
+            <i-row type="flex" justify="space-between">
+                <i-col span="11">
+                    <i-form-item label="夹具代码">
+                        <i-input v-model="formData.Code"/>
+                    </i-form-item>
+                </i-col>
+                <i-col span="11">
+                    <i-form-item label="夹具名称">
+                        <i-input v-model="formData.Name"/>
+                    </i-form-item>
+                </i-col>
+            </i-row>
+            <i-row type="flex" justify="space-between">
+                <i-col span="11">
+                    <i-form-item label="所属大类">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+                <i-col span="11">
+                    <i-form-item label="夹具模组">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+            </i-row>
+            <i-row type="flex" justify="space-between">
+                <i-col span="11">
+                    <i-form-item label="夹具料号">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+                <i-col span="11">
+                    <i-form-item label="配备的数量">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+            </i-row>
+            <i-row type="flex" justify="space-between">
+                <i-col span="24">
+                    <i-form-item label="用途">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+            </i-row>
+            <i-row type="flex" justify="space-between">
+                <i-col span="11">
+                    <i-form-item label="保养点检周期">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+                <i-col span="11">
+                    <i-form-item label="责任人">
+                        <i-input/>
+                    </i-form-item>
+                </i-col>
+            </i-row>
+        </i-form>
+    </i-row>
 </template>
 <script>
+let axios = require("axios");
 export default {
+    props: {
+        formData: {
+            required: true,
+            type: Object
+        }
+    },
+    data () {
+        return {}
+    },
+    methods: {
+        submit (WorkcellId, callback) {
+            axios.post("/api/fwwb/SaveFixDef", {WorkcellId, ...this.formData}, msg => {
+                this.resetFields();
+                if (msg.success) {
+                    callback();
+                } else {
+                    this.$Message.warning(msg.msg);
+                }
+            });
+        }
+    }
 }
 </script>
